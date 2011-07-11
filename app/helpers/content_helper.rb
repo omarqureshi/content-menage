40158e600f25584549a2da50c598d710296d62b7
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-07-11 15:54:45 +0100

    use equality for ALL types

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,19 +1,19 @@
 module ContentHelper
   
   def show_content_field(field, builder)
-    if field.type.kind_of? String
+    if field.type == String
       if @content.class.text_area_fields.include?(field.name.to_sym)
         builder.text_area field.name
       else
         builder.text_field field.name
       end
-    elsif field.type.kind_of? Time
+    elsif field.type == Time
       builder.datetime_select(field.name, 
                               :time_separator => "<span class=\"separator\">:</span>".html_safe, 
                               :date_separator => "<span class=\"separator\">&mdash;</span>".html_safe, 
                               :datetime_separator => "",
                               :include_blank => (field.name != "publish_date")) + content_tag(:span, "UTC", :class => "time-zone separator")
-    elsif field.type.kind_of? Boolean
+    elsif field.type == Boolean
       builder.check_box field.name
     elsif field.type == Integer
       builder.select(field.name, options_for_select((Content::MIN_PRIORITY..Content::MAX_PRIORITY).to_a, @content.send(field.name) || Content::DEFAULT_PRIORITY))
