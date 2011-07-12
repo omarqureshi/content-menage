7a593cc67498cc697b27ba52099960cd8f3b0ed6
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-07-12 17:17:42 +0100

    Javascript changes

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+// FIXME: Tell people that this is a manifest file, real code should go into discrete files
+// FIXME: Tell people how Sprockets and CoffeeScript works
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.formalize.min
+//= require fancybox/jquery.fancybox-1.3.4.pack
+//= require fancybox/jquery.easing-1.3.pack
+//= require site
