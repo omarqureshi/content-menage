3cff39ca39701b46d11f8d15ef2583ef8a0e9308
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-07-15 21:15:20 +0100

    Downlodable controller

--- app/controllers/downloadables_controller.rb
+++ app/controllers/downloadables_controller.rb
@@ -1,2 +1,24 @@
 class DownloadablesController < ApplicationController
+  before_filter :ensure_logged_in
+  
+  def create
+    @content = get_content
+    @downloadable = get_downloadable
+    @downloadable.attributes = params[:downlodable]
+    @downloadable.save
+    redirect_to edit_content_path(@content)
+  end
+  
+  private
+  
+    def get_content
+      Content.first(:conditions => {:slug => params[:content_id]})
+    end
+    
+    def get_downloadable
+      if @content
+        @content.downloadables.first || @content.downloadables.build
+      end
+    end
+  
 end
